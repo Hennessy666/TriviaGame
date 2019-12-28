806409a7cecd3ba038096bbeb1b956aa78115f03
github_Hennessy666 <sonjahennessy@gmail.com>
2019-12-27 23:18:34 -0500

    Created folders for css, javascript and images

--- Assets/css/app.js
+++ Assets/css/app.js
@@ -1,36 +1,37 @@
 var card = $("#quiz-area");
+var countStartNumber = 30;
 
 //Question set
 var questions = [
     {
         question: "What color is a giraffe's tongue?"
         answers: ["Red", "Blue", "Black", "Green"]
-        correctAnswer: "Blue"
+        correctAnswer: "Blue",
     },
     {
         question: "How many hours a day does an elephant eat?"
         answers: ["20", "2", "15", "1"]
-        correctAnswer: "20"
+        correctAnswer: "20",
     },
     {
         question: "What are baby sharks called?"
         answers: ["Calfs", "Pups", "Whales", "Tigers"]
-        correctAnswer: "Pups"
+        correctAnswer: "Pups",
     },
     {
         question: "The largest African terrestrial animal is?"
         answers: ["Hippos", "Giraffes", "Ostrich", "Elephants"]
-        correctAnswer: "Elephants"
+        correctAnswer: "Elephants",
     },
     {
         question: "Which animal does not have sweat glands?"
         answers: ["African wild dog", "Gorillas", "Wildebeest", "Elephants"]
-        correctAnswer: "Elephants"
+        correctAnswer: "Elephants",
     },
     {
         question: "How long do Gorillas live?"
         answers: ["Roughly 10 years", "Roughly 35 years", "Roughly 23 years", "Roughly 50 years"]
-        correctAnswer: "Roughly 35 years"
+        correctAnswer: "Roughly 35 years",
     },
 
 ];
